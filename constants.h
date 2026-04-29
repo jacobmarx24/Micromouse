457143dea644effdcefcff573cac9ec2e653834b
jacobmarx24 <jacob.marx24@gmail.com>
2026-04-28 21:18:07 -0500

    Added template code

---
+++ constants.h
@@ -0,0 +1,36 @@
+/*
+* constants.h
+ *
+ *  Created on: Nov 12, 2025
+ *      Author: bill_booth
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+struct Color{
+    int r,g,b;
+};
+
+struct Parameter{
+    set<pair<int,int> >* walls;
+    SDL_Plotter* g;
+};
+
+//const int row = 40;
+//const int col = 70;
+
+const int row = 20;
+const int col = 35;
+
+const int block   = 22;
+const int buffer  = 100;
+const int padding = 6;
+const int wallThickness = 3; // must be odd
+
+
+
+
+
+
+#endif /* CONSTANTS_H_ */
